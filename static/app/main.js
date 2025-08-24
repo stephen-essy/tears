b73aa54dcf809e4d65299f630e1f72d494f2f201
Stephen-essy <stephenesau02@gmail.com>
2025-08-24 13:25:09 +0300

    log out implementation

--- static/app/main.js
+++ static/app/main.js
@@ -1,25 +1,13 @@
 import { AuthStorage } from "./authentication.js";
 const user = AuthStorage.get();
 let name = user.name;
+let signOutBtn=document.getElementById('sign-out')
 const user_name_display = document.getElementById("logo-name");
-let button_to_webSocket=document.getElementById('connect')
-let stompClient = null;
 if (name) {
   user_name_display.innerHTML = name;
 }
-function connect() {
-  const socket = new SockJS("http://172.16.130.195:8080/laughter");
-  stompClient = Stomp.over(socket);
-  stompClient.connect(
-    {},
-    (frame) => {
-      console.log("Connected :" + frame);
-      document.getElementById("status").innerText = "Connected !";
-    },
-    (error) => {
-      console.log("Connection error :" + error);
-      document.getElementById("status").innerText = "connection failed !";
-    }
-  );
-}
-button_to_webSocket.addEventListener('click',connect)
+signOutBtn.addEventListener('click',()=>{
+  window.location.href="authentication.html";
+  AuthStorage.clear();
+})
+
