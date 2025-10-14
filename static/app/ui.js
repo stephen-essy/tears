38047393afd1ff9c28507ac5b2e6b2bfe6c37238
Stephen-essy <stephenesau02@gmail.com>
2025-10-14 23:18:57 +0300

    new improvements of user interface nothing great but shit

--- static/app/ui.js
+++ static/app/ui.js
@@ -1,8 +1,10 @@
-import { AuthStorage } from "./authentication.js";
+import { AuthStorage, isTokenExpired } from "./authentication.js";
 const user = AuthStorage.get();
 const logOutButton = document.getElementById("sign-out");
 const queenLeaves = document.getElementById("sign-out-button");
-const event_submitter=document.getElementById('event-submitter')
+const event_submitter = document.getElementById("event-submitter");
+const endpoint = "127.0.0.1";
+const submitter_hero=document.getElementById('event-submitter-hero');
 $(document).ready(() => {
   let isDark = true;
   let theme = $(".dark");
@@ -48,8 +50,7 @@ export function updateClock() {
     second: "2-digit",
     hour12: true,
   });
-
-  clock.textContent=timeString;
+  clock.textContent = timeString;
 }
 
 export function showDay() {
@@ -91,7 +92,7 @@ queenLeaves.addEventListener("click", (event) => {
   const alertCard = document.getElementById("alert-card");
   const messageCard = document.getElementById("alert-card-message");
   alertCard.style.opacity = "0";
-  alert("Thank you for you confrimation",'success');
+  alert("Thank you for you confrimation", "success");
   // messageCard.innerText = "thanks for your confrimation";
   AuthStorage.clear();
   setTimeout(() => {
@@ -99,7 +100,7 @@ queenLeaves.addEventListener("click", (event) => {
   }, 2000);
 });
 
-async function createProfile(event) {
+export async function createProfile(event) {
   event.preventDefault();
 
   let profile = {
@@ -118,7 +119,10 @@ async function createProfile(event) {
     alert("Please Enter all fields", "error");
     return;
   }
-
+  if (isTokenExpired(user)) {
+    alert("Session Expired Re-sign in", "error");
+    return;
+  }
   try {
     let request = await fetch("http://localhost:8080/laughter/profile/add", {
       method: "POST",
@@ -131,21 +135,61 @@ async function createProfile(event) {
 
     let response = await request.json();
     if (request.ok) {
-      alert(response.message,'success')
+      alert(response.message, "success");
     } else {
-      alert(response.message,'error')
+      alert(response.message, "error");
     }
   } catch (error) {
-    alert('Error in processing','error')
+    alert("Error in processing", "error");
   }
 }
 
-async function submitEvent(){
-  let event={
-    
+async function submitEvent(e) {
+  e.preventDefault();
+  let event = {
+    name: document.getElementById("event-name").value.trim(),
+    startTime: document.getElementById("start-time").value.trim(),
+    endTime: document.getElementById("end-time").value.trim(),
+    date: document.getElementById("date").value.trim(),
+    location: document.getElementById("location").value.trim(),
+  };
+  if (
+    !event.name ||
+    !event.startTime ||
+    !event.endTime ||
+    !event.endTime ||
+    !event.date ||
+    !event.location
+  ) {
+    alert("please fill all fields", "error");
+    console.log("event incomplete");
+    return;
+  }
+  if (isTokenExpired(user)) {
+    alert("Session Expired Re-sign in", "error");
+    return;
+  }
+  try {
+    let request = await fetch(`http://${endpoint}:8080/laughter/event/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+      body: JSON.stringify(event),
+    });
+
+    let response = await request.json();
+    if (request.ok) {
+      alert(`${response.message}`, "success");
+    } else {
+      alert(`${response.message}`, "error");
+    }
+  } catch (error) {
+    alert("Error in processing", "error");
+    console.log(error);
   }
 }
-  
 
 if (user.profile === null) {
   document.getElementById("update-create").textContent = "create";
@@ -169,13 +213,22 @@ if (user.profile === null) {
   submitProfile.addEventListener("click", createProfile);
 }
 
-event_submitter.addEventListener("click",(e)=>{
-  e.preventDefault()
-  alert("cooking something",'success');
-  console.log("hello there");
-})
+event_submitter.addEventListener("click", submitEvent);
+console.log("Expire Status :" + isTokenExpired(user));
+submitter_hero.addEventListener("click", (event) => {
+  event.preventDefault();
+  const modal = document.getElementById("profile-modal");
+  const event_modal = document.getElementById("event-creation");
+  const profile_modal = document.getElementById("profile-creation");
+  const event_planned = document.getElementById("planned-event").value.trim();
+  modal.style.display = "block";
+  profile_modal.style.display = "none";
+  event_modal.style.display = "flex";
+  document.getElementById("event-name").value = event_planned;
+  event_planned.value='';
+});
 showUserName();
-showUserProfile();
 updateClock();
 setInterval(updateClock, 1000);
+showUserProfile();
 showDay();
